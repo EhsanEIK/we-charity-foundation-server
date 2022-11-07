44d28066f6fde75ac07c27f667f7114d44106dd6
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 14:29:10 +0600

    added single activity by Id

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,6 +23,15 @@ async function run() {
             const query = {};
             const activities = await activitiesCollection.find(query).toArray();
             res.send(activities);
+        })
+
+        // activity [GET one]
+        app.get('/activities/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const activity = await activitiesCollection.findOne(query);
+            res.send(activity);
         })
 
         // activity [POST]
