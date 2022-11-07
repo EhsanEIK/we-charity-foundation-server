e9004a26f90970fe4c2dae36f1b31c870e3078d4
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 17:54:33 +0600

    added jwt api to make the token and send it to the client side

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ async function run() {
 
         // JWT
         app.post('/jwt', (req, res) => {
-            const user = req.body.currentUser;
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ token });
         })
 
         // activity [GET all]
