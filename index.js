34fc506dcf49c08d99ea5f3bf825b7f24c3a382b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 11:10:58 +0600

    added database configuration

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
+require('dotenv').config();
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send("We-The Charity Server is running");
