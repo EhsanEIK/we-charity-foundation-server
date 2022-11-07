29c65e98c9c380068e1b93fac0b486232d6a8744
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 11:26:42 +0600

    added POST method to create a new activity on database

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 require('dotenv').config();
+
+// middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const activitiesCollection = client.db('weCharityDB').collection('activities');
+
+        // activity [POST]
+        app.post('/activities', async (req, res) => {
+            const activity = req.body;
+            const result = await activitiesCollection.insertOne(activity);
+            res.send(result);
+        })
+    }
+    finally { }
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send("We-The Charity Server is running");
