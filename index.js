1536ec4b232d07593ee03b71469ec4048745e592
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 18:24:20 +0600

    added jwt verification

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -34,7 +49,7 @@ async function run() {
         })
 
         // activity [GET one]
-        app.get('/activities/:id', async (req, res) => {
+        app.get('/activities/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const activity = await activitiesCollection.findOne(query);
@@ -42,14 +57,17 @@ async function run() {
         })
 
         // activity [POST]
-        app.post('/activities', async (req, res) => {
+        app.post('/activities', verifyJWT, async (req, res) => {
             const activity = req.body;
             const result = await activitiesCollection.insertOne(activity);
             res.send(result);
         })
 
         // my activity cart [GET]
-        app.get('/myActivities', async (req, res) => {
+        app.get('/myActivities', verifyJWT, async (req, res) => {
+            if (req.decoded.currentUser !== req.query.email) {
+                return res.status(403).send({ message: 'unauthorized access' });
+            }
             let query = {};
             if (req.query.email) {
                 query = { email: req.query.email };
@@ -59,14 +77,14 @@ async function run() {
         })
 
         // my activity cart [POST]
-        app.post('/myActivities', async (req, res) => {
+        app.post('/myActivities', verifyJWT, async (req, res) => {
             const myActiviity = req.body;
             const result = await myActivitiesCollection.insertOne(myActiviity);
             res.send(result);
         })
 
         // my activity cart [DELETE]
-        app.delete('/myActivities/:id', async (req, res) => {
+        app.delete('/myActivities/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await myActivitiesCollection.deleteOne(query);
